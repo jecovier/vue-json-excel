866e9019990597aab2267637591f1f67de88ec63
Gustavo Ehrhardt <gus.ehr@gmail.com>
2018-03-02 11:11:02 -0300

    change to default indentation

--- JsonExcel.vue
+++ JsonExcel.vue
@@ -175,28 +175,28 @@ export default {
 			}
 			return keys
 		},
-        callItemCallback: function(field, itemValue) {
-            if (typeof field === 'object' && typeof field.callback === 'function') {
-                return field.callback(itemValue);
-            }
+		callItemCallback: function(field, itemValue) {
+			if (typeof field === 'object' && typeof field.callback === 'function') {
+				return field.callback(itemValue);
+			}
 
-            return itemValue;
-        },
-        getNestedData: function(key, item) {
-            const field = (typeof key === 'object') ? key.field : key;
+			return itemValue;
+		},
+		getNestedData: function(key, item) {
+			const field = (typeof key === 'object') ? key.field : key;
 
-            let valueFromNestedKey = null
-            let keyNestedSplit = field.split(".")
+			let valueFromNestedKey = null
+			let keyNestedSplit = field.split(".")
 
-            valueFromNestedKey = item[keyNestedSplit[0]]
-            for (let j = 1; j < keyNestedSplit.length; j++) {
-                valueFromNestedKey = valueFromNestedKey[keyNestedSplit[j]]
-            }
+			valueFromNestedKey = item[keyNestedSplit[0]]
+			for (let j = 1; j < keyNestedSplit.length; j++) {
+				valueFromNestedKey = valueFromNestedKey[keyNestedSplit[j]]
+			}
 
-            valueFromNestedKey = this.callItemCallback(key, valueFromNestedKey);
+			valueFromNestedKey = this.callItemCallback(key, valueFromNestedKey);
 
-            return valueFromNestedKey;
-        },
+			return valueFromNestedKey;
+		},
 		base64ToBlob: function (data, mime) {
 			let base64 = window.btoa(window.unescape(encodeURIComponent(data)))
 			let bstr   = atob(base64)
